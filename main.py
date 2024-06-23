4e4970837591066685398916e1c2a4af9dc8e664
Renan Carvalho <recarvalhodesouza@gmail.com>
2024-06-23 14:56:43 -0300

    Criar funcao auxiliar

--- main.py
+++ main.py
@@ -15,43 +15,25 @@ CSV_PROCESSAMENTO = "http://vitibrasil.cnpuv.embrapa.br/download/ProcessaVinifer
 def read_root():
     return {"message": "Welcome to the Embrapa viticulture API"}
 
+def fetch_and_process_csv(url: str):
+    response = requests.get(url)
+    response.raise_for_status()  # Levanta um erro se a requisição falhar
+    data = pd.read_csv(io.StringIO(response.text), sep=';')
+    data = data.where(pd.notnull(data), None)
+    return data.to_dict(orient='records')
+
 @app.get("/producao")
 def get_data_producao():
     try:
-        # Fazer o download do CSV
-        response = requests.get(CSV_PRODUCAO)
-        response.raise_for_status()  # Levanta um erro se a requisição falhar
-
-        data = pd.read_csv(io.StringIO(response.text), sep=';')
-
-        # Preencher os valores NaN com None
-        data = data.where(pd.notnull(data), None)
-
-        data_dict = data.to_dict(orient='records')
-
+        data_dict = fetch_and_process_csv(CSV_PRODUCAO)
         return {"data": data_dict}
-
-        #return response.content
     except Exception as e:
         return {"error": str(e)}
-        
 
 @app.get("/processamento")
 def get_data_processamento():
     try:
-        # Fazer o download do CSV
-        response = requests.get(CSV_PROCESSAMENTO)
-        response.raise_for_status()  # Levanta um erro se a requisição falhar
-
-        data = pd.read_csv(io.StringIO(response.text), sep=';')
-
-        # Preencher os valores NaN com None
-        data = data.where(pd.notnull(data), None)
-
-        data_dict = data.to_dict(orient='records')
-
+        data_dict = fetch_and_process_csv(CSV_PROCESSAMENTO)
         return {"data": data_dict}
-
-        #return response.content
     except Exception as e:
         return {"error": str(e)}
